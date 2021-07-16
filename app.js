247af61ac7562d640b1843727e45c8f9c548cbb3
Raul Ivan <ra04navi@outlook.com>
2021-07-16 01:44:14 -0500

    El proyecto se convirtio a NodeJS para poder hacer uso del backend con la base de datos

---
+++ app.js
@@ -0,0 +1,29 @@
+//Se llama al modulo de express para poder usarlo
+const express = require('express');
+const app = express();
+
+//Se establece una vista con ejs
+app.set('view engine', 'ejs');
+
+//Establecemos la ruta predeterminada para los recursos
+app.use(express.static('public'));
+app.use('/resources', express.static(__dirname+'/public'));
+
+//Se establece la ruta de cada vista del proyecto
+app.get('/',(req,res) => {
+    res.render('index');
+});
+app.get('/citas',(req,res) => {
+    res.render('citas');
+});
+app.get('/contacto',(req,res) => {
+    res.render('contacto');
+});
+app.get('/login',(req,res) => {
+    res.render('login');
+});
+
+//Se inicializa el servidor
+app.listen(3000,(req,res) => {
+    console.log('SERVIDOR INICIADO EN PORT 3000');
+});
